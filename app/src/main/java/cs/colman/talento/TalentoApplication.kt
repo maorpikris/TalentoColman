59efb922b307db9fb3d148d043e7dd73dbb05597
Maor Pikris <maorp1111@gmail.com>
2026-03-27 01:21:49 +0000

    Add Room database setup and data access objects

--- app/src/main/java/cs/colman/talento/TalentoApplication.kt
+++ app/src/main/java/cs/colman/talento/TalentoApplication.kt
@@ -2,10 +2,14 @@ package cs.colman.talento
 
 import android.app.Application
 import android.util.Log
+import cs.colman.talento.data.local.AppDatabase
 
 const val TAG = "TalentoApplication"
 
 class TalentoApplication : Application() {
+
+    val database by lazy { AppDatabase.getDatabase(this) }
+
     override fun onCreate() {
         super.onCreate()
     }
