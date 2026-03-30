873b095036d3aadec635be9ae4a3f038cd2be2d5
Ofek Berko <ofek607@gmail.com>
2026-03-30 14:27:16 +0000

    Add navigation graph and splash fragment layout with system bar padding

--- app/src/main/java/cs/colman/talento/MainActivity.kt
+++ app/src/main/java/cs/colman/talento/MainActivity.kt
@@ -16,5 +16,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
     }
 }
